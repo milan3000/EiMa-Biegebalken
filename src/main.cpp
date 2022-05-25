8c32da5a7d140b7d785e8ca60672596edc7b5ae7
milan3000 <milanw12@gmail.com>
2022-05-25 12:55:51 +0200

    defined basic variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
-int stepsPerRevolution = 800;
-int microSteppingFactor = 1;
-int trackLength = 350; //in milimeters
-int lengthResolution = 1; //in milimeters
+int pinSensor = A0;
+int pinButton = 8;
 
-int minimumDistance = 30; //in milimeters
+const int stepsPerRevolution = 800;
+const int microSteppingFactor = 1;
+const int spindlePitch = 3; //in mm
+const int trackLength = 350; //in mm
+const float resolution = 1; //in mm
+int numberOfDataPoints = trackLength/resolution;
+int stepsPerDataPoint = resolution * (stepsPerRevolution / spindlePitch);
+
+int xAxisZeroOffset = 20; //in mm
+int xAxisIndex = 0; 
+
+int minimumDistance = 30; //in mm
 float measurement = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 bool checkForButtonPress(){
@@ -21,8 +30,25 @@ void moveToStartPosition(){
 
 }
 
+void measureDistance(){
+
+}
+
+void sendDistanceOverSerial(){
+
+}
+
+void moveStepper(){
+
+}
+
 void runMeasurementSequence(){
   moveToStartPosition();
+  while(xAxisIndex < numberOfDataPoints){
+    measureDistance();
+    sendDistanceOverSerial();
+    moveStepper();
+  }
 }
 
 void loop() {
